acb4b9c0eadf607d881e9dabcbec0b9c90795678
Quinten De Swaef <quintendeswaef@gmail.com>
2014-09-26 11:58:59 +0200

    upgrade to spring boot 1.1.7 fixed tests

--- src/test/java/com/deswaef/spring/examples/profiles/ProductionProfileApplicationTest.java
+++ src/test/java/com/deswaef/spring/examples/profiles/ProductionProfileApplicationTest.java
@@ -1,10 +1,10 @@
 package com.deswaef.spring.examples.profiles;
 
-import com.deswaef.spring.examples.profiles.ProfileApplication;
 import com.deswaef.spring.examples.profiles.mvc.GreetController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.IntegrationTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -21,6 +21,7 @@ import static org.fest.assertions.Assertions.assertThat;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ActiveProfiles("production")
 @ContextConfiguration(classes = {ProfileApplication.class})
+@IntegrationTest("server.port:0")
 public class ProductionProfileApplicationTest {
 
     @Autowired
